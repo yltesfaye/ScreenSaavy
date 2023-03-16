7843c815ff6de89cc5999941ce005d536ac9aab0
Wael Delessa <waeltesfaye123@gmail.com>
2023-03-15 23:16:35 -0500

    connected back-end to front end, added movie.html/js

--- dao/reviewsDAO.js
+++ dao/reviewsDAO.js
@@ -18,7 +18,7 @@ export default class ReviewDAO {
 
     static async addReview(movieId, user, review) {
         try{
-            const reviewDoc{
+            const reviewDoc = {
                 movieId: movieId,
                 user: user,
                 review: review,
@@ -27,6 +27,54 @@ export default class ReviewDAO {
             return await reviews.insertOne(reviewDoc)
         } catch(e) {
             console.error(`Unable to post review: ${e}`)
+            return { error: e}
+        }
+    }
+
+    static async getReview(reviewID) {
+        try {
+            return await reviews.findOne({ _id: ObjectId(reviewID)})
+        } catch(e) {
+            console.error(`Unable to get review: ${e}`)
+            return { error: e}
+        }
+    }
+
+    static async updateReview(reviewID, user, review) {
+        try{
+            const updateResponse = await reviews.updateOne(
+                { _id: ObjectId(reviewID)},
+                { $set: {user: user, review: review}}
+            )
+
+            return updateResponse
+        } catch(e) {
+            console.error(`Unable to update review: ${e}`)
+            return { error: e}
+        }
+    }
+
+    static async deleteReview(reviewId) {
+        try{
+            const deleteResponse = await reviews.deleteOne({
+                _id: ObjectId(reviewId),
+            })
+
+            return deleteResponse
+        } catch(e) {
+            console.error(`Unable to delete review: ${e}`)
+            return { error: e}
+        }
+
+    }
+
+    static async getReviewsByMovieId(movieId) {
+        try{
+            const cursor = await reviews.find({
+                movieId: parseInt(movieId)
+            })
+            return cursor.toArray()
+        } catch(e) {
             return { error: e}
         }
     }
